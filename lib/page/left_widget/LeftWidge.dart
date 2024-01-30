3e3d0f7251ee360a07c144bcf4798003d6404168
SQ <59799517@qqcom>
2024-01-30 11:56:50 +0800

    增加插件下载到服务器功能-更新版本

--- lib/page/left_widget/LeftWidge.dart
+++ lib/page/left_widget/LeftWidge.dart
@@ -141,7 +141,7 @@ class _LeftWidgeState extends State<LeftWidge> {
                     text: TextSpan(
                       children: [
                         TextSpan(
-                          text:'版本号：v1.1.4',
+                          text:'版本号：v1.1.5',
                           style: TextStyle(
                             color:  Get.isDarkMode?dark_text_Colors:light_text_Colors,
                           ),
